bf0615f7df00f95f84b50199b04349cca1a31cd8
Elkin Romero <gio.busrom@gmail.com>
2020-10-25 22:16:06 -0600

    half of part 2 done

--- decisiontree/test.py
+++ decisiontree/test.py
@@ -8,27 +8,10 @@ from tools import arff
 import itertools
 from decisiontree import DTClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 
-# data = np.array([[0,0,0],
-#         [1,1,0],
-#         [1,2,1],
-#         [0,2,1],
-#         [0,1,0],
-#         [0,1,1],
-#         [1,0,1],
-#         [0,1,0],
-#         [1,0,0]])
-# labels = np.array([[0],
-#           [1],
-#           [2],
-#           [0],
-#           [2],
-#           [0],
-#           [2],
-#           [2],
-#           [1]])
-# counts = [2,3,2,3] ## this is so you know how many types for each column
+print("*******************PART 1************************************")
 
 arff_path_train = r"training/lenses.arff"
 arff_path_test = r"training/all_lenses.arff"
@@ -49,26 +32,83 @@ data2 = mat2.data[:,0:-1]
 labels2 = mat2.data[:,-1].reshape(-1,1)
 # print(data2)
 # print(labels2)
-# pred = DTClass.predict(data2)
+pred = DTClass.predict(data2)
 Acc = DTClass.score(data2,labels2)
+
 # np.savetxt("pred_lenses.csv",pred,delimiter=",")
 print("Accuracy = [{:.2f}]".format(Acc))
 
+print("*******************EVALUATION************************************")
+arff_path_train = r"training/zoo.arff"
+arff_path_test = r"training/all_zoo.arff"
+mat = arff.Arff(arff_path_train)
 
+counts = [] ## this is so you know how many types for each column
 
-# DTClass = DTClassifier(counts)
-# DTClass.fit(data,labels)
-# score = DTClass.score(data,labels)
-# print("Score",score)
+for i in range(mat.data.shape[1]):
+       counts += [mat.unique_value_count(i)]
+data = mat.data[:,0:-1]
+labels = mat.data[:,-1].reshape(-1,1)
+# print(data)
+# print(labels)
+DTClass = DTClassifier(counts)
+DTClass.fit(data,labels)
+mat2 = arff.Arff(arff_path_test)
+data2 = mat2.data[:,0:-1]
+labels2 = mat2.data[:,-1].reshape(-1,1)
+# print(data2)
+# print(labels2)
+pred = DTClass.predict(data2)
+Acc = DTClass.score(data2,labels2)
 
+# np.savetxt("pred_zoo.csv",pred,delimiter=",")
+print("Accuracy = [{:.2f}]".format(Acc))
 
+print("*******************PART 2************************************")
 
+arff_path= r"training/cars.arff"
+mat = arff.Arff(arff_path)
 
-# data = np.array([[1,2,1,4],
-#         [1,1,1,4],
-#         [1,2,1,3]])
-# print(DTClass.noMoreAttributes(data))
-#
-# splitXindxRange = data.shape[1]-1
-# X_transposed = data[:, np.r_[splitXindxRange]]
-# print(X_transposed)
+# counts = [] ## this is so you know how many types for each column
+# for i in range(mat.data.shape[1]):
+#        counts += [mat.unique_value_count(i)]
+data2 = mat.data[:,0:-1]
+labels = mat.data[:,-1].reshape(-1,1)
+data = np.concatenate((data2, labels), axis = 1)
+# print(data)
+""" now K-folding """
+# prepare cross validation
+kfold = KFold(n_splits=10, random_state=None, shuffle=True)
+scores = []
+
+for train, test in kfold.split(data):
+    print("DATA SIZE ", len(data[train]))
+    print("TEST SIZE ", len(data[test]))
+	# print('train: %s, test: %s' % (data2[train], data2[test]))
+    columns = len(data[train][0])
+    data_transposed = data[train].T
+    counts = []
+    for column in range(0, columns):
+        unique = np.unique(data_transposed[column])
+        counts.append(len(unique))
+    print("COUNTS TRAIN",counts)
+    DTClass = DTClassifier(counts)
+    splitXindxRange = data[train].shape[1] - 1
+    labelsK = data[train][:, np.r_[splitXindxRange]]
+    dataK = data[train][:, np.r_[0:splitXindxRange]]
+    labelsTestK = data[test][:, np.r_[splitXindxRange]]
+    dataTestK = data[test][:, np.r_[0:splitXindxRange]]
+    DTClass.fit(dataK,labelsK)
+
+    columnsTest = len(data[test][0])
+    dataTest_transposed = data[test].T
+    countsTest = []
+    for column in range(0, columnsTest):
+        unique = np.unique(dataTest_transposed[column])
+        countsTest.append(len(unique))
+    print("COUNTS TEST",countsTest)
+
+    scores.append(DTClass.score(dataTestK,labelsTestK))
+
+    # break
+print("Scores ", scores)
