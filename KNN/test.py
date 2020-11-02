20b5d698adf87d017d897f93848b1f13a444890e
Elkin Romero <gio.busrom@gmail.com>
2020-11-01 18:00:33 -0700

    test script customized

--- KNN/test.py
+++ KNN/test.py
@@ -15,22 +15,21 @@ print("*******************PART 1************************************")
 arff_path_train = r"training/seismic-bumps_train.arff"
 arff_path_test = r"training/seismic-bumps_test.arff"
 mat = arff.Arff(arff_path_train)
-mat = arff.Arff(arff_path_test)
+mat2 = arff.Arff(arff_path_test)
 arrayTypes = mat.attr_types
 del arrayTypes[-1]
+# print(mat.data)
+train_data = mat.data[:,0:-1]
+train_labels = mat.data[:,-1].reshape(-1,1)
 
-raw_data = mat.data
-h,w = raw_data.shape
-train_data = raw_data[:,:-1]
-train_labels = raw_data[:,-1]
 
-raw_data2 = mat2.data
-h2,w2 = raw_data2.shape
-test_data = raw_data2[:,:-1]
-test_labels = raw_data2[:,-1]
+test_data = mat2.data[:,0:-1]
+test_labels = mat2.data[:,-1].reshape(-1,1)
 
-KNN = KNNClassifier(columntype= arrayTypes,labeltype ='classification',weight_type='inverse_distance')
+
+KNN = KNNClassifier(columntype= arrayTypes, labelType ='classification' ,weight_type='inverse_distance',k=3)
 KNN.fit(train_data,train_labels)
 pred = KNN.predict(test_data)
 score = KNN.score(test_data,test_labels)
 np.savetxt("seismic-bump-prediction.csv",pred,delimiter=',',fmt="%i")
+print(score)
