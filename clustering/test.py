64390fc7937a5682a0d06bfb6fcf6e60a1da257c
Elkin Romero <gio.busrom@gmail.com>
2020-11-18 21:11:01 -0700

    test.py to test skitlearn

--- clustering/test.py
+++ clustering/test.py
@@ -10,7 +10,8 @@ from clustering import HAC, Kmeans
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.preprocessing import scale
-
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
 
 print("*******************PART 1************************************")
 print("*******************DEBUG************************************")
@@ -19,14 +20,22 @@ arff_path_train = r"training/abalone.arff"
 mat = arff.Arff(arff_path_train,label_count=0)
 raw_data = mat.data
 data = raw_data
+# data = data[...,:-1]
+# print(data)
 ##Normalize the data##
 scaler = preprocessing.MinMaxScaler()
 scaler.fit(data)
 norm_data = scaler.transform(data)
 
-
+# clustering = AgglomerativeClustering(n_clusters = 5, linkage= 'single').fit(norm_data)
 # norm_data = np.array([[0.8,0.7],[-0.1,0.2],[0.9,0.8],[0,0.2],[0.2,0.1]])
+### KMEANS ###
+# KMEANS = Kmeans.KMEANSClustering(k=5,debug=True)
+# KMEANS.fit(norm_data)
+# KMEANS.save_clusters("debug_kmeans.txt")
+
 ### HAC SINGLE LINK ###
 HAC_single = HAC.HACClustering(k=5,link_type='single')
 HAC_single.fit(norm_data)
 HAC_single.save_clusters("debug_hac_single.txt")
+print(clustering.labels_)
